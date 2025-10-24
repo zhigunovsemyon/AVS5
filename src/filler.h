6af0233b2a5347a1a4323c634b9f3426e5ee692b
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-24 13:36:11 +0300

    Теперь потоки не печатают повторяющиеся числа

--- src/filler.h
+++ src/filler.h
@@ -1,9 +1,10 @@
 #pragma once
+#include <bit>
 #include <omp.h>
 #include <random>
 #include <span>
 
-template <std::floating_point T> void random_array_fill(std::span<T> array, std::mt19937_64 local_rand, T min, T max)
+template <std::floating_point T> void random_array_fill(std::span<T> array, T min, T max)
 {
 	auto const arr_size = array.size();
 
@@ -11,13 +12,19 @@ template <std::floating_point T> void random_array_fill(std::span<T> array, std:
 		std::swap(min, max);
 	}
 
-#pragma omp parallel for shared(array, arr_size) private(local_rand)
-	for (std::size_t i = 0; i < arr_size; ++i) {
-		array[i] = std::uniform_real_distribution<T>{min, max}(local_rand);
+	unsigned long base_seed = std::random_device{}();
+
+#pragma omp parallel shared(array, arr_size, base_seed)
+	{
+		std::mt19937_64 local_rand{(unsigned long)omp_get_thread_num() + base_seed};
+#pragma omp for
+		for (std::size_t i = 0; i < arr_size; ++i) {
+			array[i] = std::uniform_real_distribution<T>{min, max}(local_rand);
+		}
 	}
 }
 
-template <std::integral T> void random_array_fill(std::span<T> array, std::mt19937_64 local_rand, T min, T max)
+template <std::integral T> void random_array_fill(std::span<T> array, T min, T max)
 {
 	auto const arr_size = array.size();
 
@@ -25,8 +32,14 @@ template <std::integral T> void random_array_fill(std::span<T> array, std::mt199
 		std::swap(min, max);
 	}
 
-#pragma omp parallel for shared(array, arr_size) private(local_rand)
-	for (std::size_t i = 0; i < arr_size; ++i) {
-		array[i] = std::uniform_int_distribution<T>{min, max}(local_rand);
+	unsigned long base_seed = std::random_device{}();
+
+#pragma omp parallel shared(array, arr_size, base_seed)
+	{
+		std::mt19937_64 local_rand{(unsigned long)omp_get_thread_num() - base_seed};
+#pragma omp for
+		for (std::size_t i = 0; i < arr_size; ++i) {
+			array[i] = std::uniform_int_distribution<T>{min, max}(local_rand);
+		}
 	}
 }
--- src/main.cc
+++ src/main.cc
@@ -11,12 +11,10 @@ concept SortConcept = std::copy_constructible<T> && std::three_way_comparable<T>
 
 int main()
 {
-	constexpr std::size_t arr_size = 100;
+	constexpr std::size_t arr_size = 50;
 	std::vector<double> nums(arr_size);
 
-	std::mt19937_64 rand_eng{std::random_device{}()};
-
-	random_array_fill(std::span{nums}, rand_eng, 11200.4, 120.0);
+	random_array_fill(std::span{nums}, 1120.0, 1.200);
 	print_array(std::span{nums});
 	return 0;
 }
