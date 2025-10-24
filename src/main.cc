7b0ce77122e7dc3688f6445ba94a2684c715025b
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-24 11:57:44 +0300

    init commit

---
+++ src/main.cc
@@ -0,0 +1,70 @@
+#include <compare>
+#include <cstddef>
+#include <omp.h>
+#include <print>
+#include <random>
+#include <span>
+#include <utility>
+#include <vector>
+
+template <class T>
+concept SortConcept = std::copy_constructible<T> && std::three_way_comparable<T>;
+
+std::mt19937_64 g_rand{std::random_device{}()};
+
+template <std::floating_point T> void random_array_fill(std::span<T> array, T min, T max)
+{
+	auto const arr_size = array.size();
+	auto local_rand = g_rand;
+
+	if (min > max) {
+		std::swap(min, max);
+	}
+	auto dist = std::uniform_real_distribution<T>(min, max);
+
+#pragma omp parallel for shared(array, arr_size, dist) private(local_rand)
+	for (std::size_t i = 0; i < arr_size; ++i) {
+		array[i] = dist(local_rand);
+	}
+}
+
+template <std::integral T> void random_array_fill(std::span<T> array, T min, T max)
+{
+	auto const arr_size = array.size();
+	auto local_rand = g_rand;
+
+	if (min > max) {
+		std::swap(min, max);
+	}
+	auto dist = std::uniform_int_distribution<T>(min, max);
+
+#pragma omp parallel for shared(array, arr_size, dist) private(local_rand)
+	for (std::size_t i = 0; i < arr_size; ++i) {
+		array[i] = dist(local_rand);
+	}
+}
+
+template <std::floating_point T> void print_array(std::span<T> const array)
+{
+	for (auto const & n : array) {
+		std::print("{:.5f}; ", n);
+	}
+	std::println();
+}
+
+template <std::integral T> void print_array(std::span<T> const array)
+{
+	for (auto const & n : array) {
+		std::print("{} ", n);
+	}
+	std::println();
+}
+
+int main()
+{
+	constexpr std::size_t arr_size = 100;
+	std::vector<double> nums(arr_size);
+	random_array_fill(std::span{nums}, 11200.4, 120.0);
+	print_array(std::span{nums});
+	return 0;
+}
