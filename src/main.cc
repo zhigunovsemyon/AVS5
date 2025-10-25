6eb4c1ff483fa973177c9d542a90ff7adf29788e
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-25 23:03:20 +0300

    Фиксы для работы в винде

--- src/main.cc
+++ src/main.cc
@@ -14,9 +14,9 @@ concept SortConcept = std::copy_constructible<T> && std::three_way_comparable<T>
 template <class T> void sum_arrays(std::span<T> const vec1, std::span<T> const vec2, std::span<T> vec3)
 {
 	assert(vec1.size() == vec2.size() && vec2.size() == vec3.size());
-	long mid_point = vec1.size() / 2;
+	auto mid_point = static_cast<std::int64_t>(vec1.size()) / 2;
 
-#pragma omp parallel sections //потоков не будет больше двух
+#pragma omp parallel sections // потоков не будет больше двух
 	{
 
 #pragma omp section
@@ -33,7 +33,7 @@ template <class T> void sum_arrays(std::span<T> const vec1, std::span<T> const v
 
 template <class T, bool no_print = false> auto task1(std::span<T> big_array)
 {
-	ssize_t subarray_size = big_array.size() / 3;
+	auto subarray_size = static_cast<std::int64_t>(big_array.size()) / 3;
 	auto p0 = big_array.begin();
 	auto p1 = p0 + subarray_size;
 	auto p2 = p1 + subarray_size;
@@ -42,7 +42,7 @@ template <class T, bool no_print = false> auto task1(std::span<T> big_array)
 	std::span<T> vec1{p0, p1}, vec2{p1, p2}, vec3{p2, p3};
 	sum_arrays(vec1, vec2, vec3);
 
-	if (no_print == true) {
+	if (no_print) {
 		return vec2.size();
 	}
 
@@ -70,7 +70,7 @@ int main()
 	std::println("Заполено {} числами за {:4f} секунд", arr_size, omp_get_wtime() - start_time);
 
 	start_time = omp_get_wtime();
-	std::println("Сложены массивы на {} чисел за {:4f} секунд", task1<arr_type,disable_print>(std::span{nums}),
+	std::println("Сложены массивы на {} чисел за {:4f} секунд", task1<arr_type, disable_print>(std::span{nums}),
 		     omp_get_wtime() - start_time);
 
 	return 0;
