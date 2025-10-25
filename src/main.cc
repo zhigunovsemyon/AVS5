c75b7b2a3dc392827d375b90f682a0bee8598851
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-25 22:51:43 +0300

    Сделал первое задание

--- src/main.cc
+++ src/main.cc
@@ -16,7 +16,7 @@ template <class T> void sum_arrays(std::span<T> const vec1, std::span<T> const v
 	assert(vec1.size() == vec2.size() && vec2.size() == vec3.size());
 	long mid_point = vec1.size() / 2;
 
-#pragma omp parallel sections num_threads(2)
+#pragma omp parallel sections //потоков не будет больше двух
 	{
 
 #pragma omp section
@@ -31,7 +31,7 @@ template <class T> void sum_arrays(std::span<T> const vec1, std::span<T> const v
 	}
 }
 
-template <class T> void task1(std::span<T> big_array)
+template <class T, bool no_print = false> auto task1(std::span<T> big_array)
 {
 	ssize_t subarray_size = big_array.size() / 3;
 	auto p0 = big_array.begin();
@@ -42,20 +42,36 @@ template <class T> void task1(std::span<T> big_array)
 	std::span<T> vec1{p0, p1}, vec2{p1, p2}, vec3{p2, p3};
 	sum_arrays(vec1, vec2, vec3);
 
+	if (no_print == true) {
+		return vec2.size();
+	}
+
 	std::println("Первый массив:");
 	print_array(std::span{vec1});
 	std::println("Второй массив:");
 	print_array(std::span{vec2});
 	std::println("Третий массив:");
 	print_array(std::span{vec3});
+	return vec2.size();
 }
 
 int main()
 {
-	constexpr std::size_t arr_size = 100;
-	std::vector<double> nums(arr_size);
+	using arr_type = int;
+	constexpr arr_type begin = 10;
+	constexpr arr_type end = -10;
+	constexpr bool disable_print = false;
+	constexpr std::size_t arr_size = 27;
 
-	random_array_fill(std::span{nums}, -1.0, 1.0);
-	task1(std::span{nums});
+	std::vector<arr_type> nums(arr_size);
+
+	double start_time = omp_get_wtime();
+	random_array_fill(std::span{nums}, begin, end);
+	std::println("Заполено {} числами за {:4f} секунд", arr_size, omp_get_wtime() - start_time);
+
+	start_time = omp_get_wtime();
+	std::println("Сложены массивы на {} чисел за {:4f} секунд", task1<arr_type,disable_print>(std::span{nums}),
+		     omp_get_wtime() - start_time);
+
 	return 0;
 }
