ec614422bd58162f0cc56b074b860fd375b2c86e
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-26 09:57:41 +0300

    Расставил подсчёт времени, чтобы он не выкидывался компилятором

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
-#include "task1.h"
 #include "filler.h"
+#include "task1.h"
+#include "task2.h"
 
 #include <vector>
 
@@ -11,18 +12,25 @@ int main()
 	using arr_type = int;
 	constexpr arr_type begin = 10;
 	constexpr arr_type end = -10;
-	constexpr bool disable_print = false;
-	constexpr std::size_t arr_size = 27;
+	constexpr bool disable_print = true;
+	constexpr std::size_t arr_size = 1'207'000'000;
 
 	std::vector<arr_type> nums(arr_size);
 
-	double start_time = omp_get_wtime();
+	double new_time, start_time = omp_get_wtime();
 	random_array_fill(std::span{nums}, begin, end);
-	std::println("Заполено {} числами за {:4f} секунд", arr_size, omp_get_wtime() - start_time);
+	new_time = omp_get_wtime();
+	std::println("Заполено {} числами за {:4f} секунд", arr_size, new_time - start_time);
+	start_time = new_time;
 
-	start_time = omp_get_wtime();
-	std::println("Сложены массивы на {} чисел за {:4f} секунд", task1<arr_type, disable_print>(std::span{nums}),
-		     omp_get_wtime() - start_time);
+	auto ret = task1<arr_type, disable_print>(std::span{nums});
+	new_time = omp_get_wtime();
+	std::println("Сложены массивы на {} чисел за {:4f} секунд", ret, new_time - start_time);
+	start_time = new_time;
+
+	ret = task2<arr_type, disable_print>(std::span{nums}, std::less{});
+	new_time = omp_get_wtime();
+	std::println("Рассортирован слиянием массив на {} чисел за {:4f} секунд", ret, new_time - start_time);
 
 	return 0;
 }
