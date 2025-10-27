2acabe12eb7cdab2fe49dda51ddc3bc553f1b006
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-27 19:14:09 +0300

    Взял qsort. На больших массивах падает в Stack Overflow

--- src/task3.h
+++ src/task3.h
@@ -5,12 +5,51 @@
 #include <print>
 #include <span>
 #include <vector>
+#include <cassert>
 #include <omp.h>
+
+template <typename T, typename sorter> auto qsort_key_pick(std::span<T> arr, sorter sort_type) 
+{
+	return arr.size() / 2;
+	(void)sort_type;
+}
+
+template <typename T, typename sorter>
+auto qsort_partition(std::span<T> arr, sorter sort_type)
+{
+	int64_t iend = (int64_t)arr.size() - 1;
+		
+	int64_t ikey = qsort_key_pick(arr, sort_type);
+	std::swap(arr[ikey], arr[iend]);
+	ikey = iend--;
+
+	int64_t j = -1;
+	int64_t i;
+	for (i = 0; i < ikey; ++i) {
+
+		//при проходе по большей половине всегда будет это
+		if (sort_type(arr[ikey], arr[i])) {
+			continue;
+		} 
+		//else:
+		std::swap(arr[++j], arr[i]);
+	}
+	// j сам перестанет расти, когда в оставшеёся половине все элементы будут больше
+	std::swap(arr[ikey], arr[j + 1]);
+		
+	return arr.begin() + (j + 1);
+}
 
 template <typename T, typename sorter>
 void quicksort(std::span<T> arr, sorter sort_type)
 {
+	if (arr.size() < 2) {
+		return;
+	}
+	auto split_it = qsort_partition(arr, sort_type);
 
+	quicksort(std::span{split_it + 1, arr.end()}, sort_type);
+	quicksort(std::span{arr.begin(), split_it}, sort_type);
 }
 
 template <typename T, bool no_print = false, typename sorter> auto task3(std::span<T> arr, sorter sort_type)
